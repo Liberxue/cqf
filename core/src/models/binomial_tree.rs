fbb14ec450d7a45cea7c48a29adc0f7d0ad2db04
Liberxue <Liberxue@gmail.com>
2024-07-27 13:54:00 +0800

    Refactor binomial tree model to use  enum for price initialization

--- core/src/models/binomial_tree.rs
+++ core/src/models/binomial_tree.rs
@@ -1,11 +1,15 @@
 use crate::models::{OptionParameters, OptionPricingModel};
-use rand_distr::num_traits::real::Real;
 
 // <https://www.kent.ac.uk/learning/documents/slas-documents/Binomial_models.pdf >
 // <https://www.le.ac.uk/users/dsgp1/COURSES/DERIVATE/BINOPTION.PDF  >
 pub struct BinomialTreeModel {
     /// Number of steps in the binomial tree model.
     pub steps: usize,
+}
+
+enum OptionType {
+    Call,
+    Put,
 }
 
 impl BinomialTreeModel {
@@ -16,6 +20,59 @@ impl BinomialTreeModel {
     /// * `steps` - Number of steps in the binomial tree model.
     pub fn new(steps: usize) -> Self {
         Self { steps }
+    }
+
+    /// Initializes the prices vector for call or put options.
+    ///
+    /// # Arguments
+    ///
+    /// * `params` - A reference to `OptionParameters` containing the parameters for the option.
+    /// * `option_type` - A value indicating the type of option (`Call` or `Put`).
+    ///
+    /// # Returns
+    ///
+    /// A vector containing the prices of the option at each node.
+    fn initialize_prices(&self, params: &OptionParameters, option_type: OptionType) -> Vec<f64> {
+        let n = self.steps; // Number of steps in the binomial tree
+        let dt = params.t / (n as f64); // Time step size
+        let u = f64::exp(params.sigma * (dt as f64).sqrt()); // Up factor
+        let d = 1.0 / u; // Down factor
+
+        // Terminal prices
+        (0..=n)
+            .map(|i| {
+                let price = params.s * u.powi((n - i) as i32) * d.powi(i as i32);
+                match option_type {
+                    OptionType::Call => (price - params.k).max(0.0),
+                    OptionType::Put => (params.k - price).max(0.0),
+                }
+            })
+            .collect()
+    }
+    /// Performs backward induction to calculate option price.
+    ///
+    /// # Arguments
+    ///
+    /// * `prices` - A mutable vector containing the prices of the option at each node.
+    /// * `params` - A reference to `OptionParameters` containing the parameters for the option.
+    ///
+    /// # Returns
+    ///
+    /// The calculated option price.
+    fn backward_induction(&self, prices: &mut Vec<f64>, params: &OptionParameters) -> f64 {
+        let n = self.steps; // Number of steps in the binomial tree
+        let dt = params.t / (n as f64); // Time step size
+        let u = f64::exp(params.sigma * (dt as f64).sqrt()); // Up factor
+        let d = 1.0 / u; // Down factor
+        let q = (f64::exp(params.r * dt as f64) - d) / (u - d); // Risk-neutral probability
+
+        for j in (0..n).rev() {
+            for i in 0..=j {
+                prices[i] =
+                    f64::exp(-params.r * dt as f64) * (q * prices[i] + (1.0 - q) * prices[i + 1]);
+            }
+        }
+        prices[0]
     }
 }
 
@@ -36,27 +93,8 @@ impl OptionPricingModel for BinomialTreeModel {
     ///
     /// The calculated call option price.
     fn call_price(&self, params: &OptionParameters) -> f64 {
-        let n = self.steps; // Number of steps in the binomial tree
-        let dt = params.t / (n as f64); // Time step size
-        let u = Real::exp(params.sigma * (dt as f64).sqrt()); // Up factor
-        let d = 1.0 / u; // Down factor
-        let q = (Real::exp(params.r * dt as f64) - d) / (u - d); // Risk-neutral probability
-        let mut prices = vec![0.0; n + 1];
-
-        // Terminal prices
-        for i in 0..=n {
-            prices[i] = (params.s * u.powi((n - i) as i32) * d.powi(i as i32) - params.k).max(0.0);
-        }
-
-        // Backward induction
-        for j in (0..n).rev() {
-            for i in 0..=j {
-                prices[i] =
-                    Real::exp(-params.r * dt as f64) * (q * prices[i] + (1.0 - q) * prices[i + 1]);
-            }
-        }
-
-        prices[0]
+        let mut prices = self.initialize_prices(params, OptionType::Call);
+        self.backward_induction(&mut prices, params)
     }
 
     /// Calculates the put option price using the binomial tree model.
@@ -69,24 +107,8 @@ impl OptionPricingModel for BinomialTreeModel {
     ///
     /// The calculated put option price.
     fn put_price(&self, params: &OptionParameters) -> f64 {
-        let n = self.steps; // Number of steps in the binomial tree
-        let dt = params.t / (n as f64); // Time step size
-        let u = Real::exp(params.sigma * (dt as f64).sqrt()); // Up factor
-        let d = 1.0 / u; // Down factor
-        let q = (Real::exp(params.r * dt as f64) - d) / (u - d); // Risk-neutral probability
-        let mut prices = vec![0.0; n + 1];
-        for i in 0..=n {
-            prices[i] = (params.k - params.s * u.powi((n - i) as i32) * d.powi(i as i32)).max(0.0);
-        }
-        // Backward induction
-        for j in (0..n).rev() {
-            for i in 0..=j {
-                prices[i] =
-                    Real::exp(-params.r * dt as f64) * (q * prices[i] + (1.0 - q) * prices[i + 1]);
-            }
-        }
-
-        prices[0]
+        let mut prices = self.initialize_prices(params, OptionType::Put);
+        self.backward_induction(&mut prices, params)
     }
 
     /// Calculates the delta of the option using the binomial tree model.
@@ -101,7 +123,7 @@ impl OptionPricingModel for BinomialTreeModel {
     fn delta(&self, params: &OptionParameters) -> f64 {
         let n = self.steps;
         let dt = params.t / (n as f64);
-        let u = Real::exp(params.sigma * (dt as f64).sqrt());
+        let u = f64::exp(params.sigma * (dt as f64).sqrt());
         let d = 1.0 / u;
 
         let up_params = OptionParameters {
@@ -136,7 +158,7 @@ impl OptionPricingModel for BinomialTreeModel {
     fn gamma(&self, params: &OptionParameters) -> f64 {
         let n = self.steps;
         let dt = params.t / (n as f64);
-        let u = Real::exp(params.sigma * (dt as f64).sqrt());
+        let u = f64::exp(params.sigma * (dt as f64).sqrt());
         let d = 1.0 / u;
 
         let delta_up = self.delta(&OptionParameters {
